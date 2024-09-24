2e344b739aa2b4e93aaafd080d020278b284dceb
Andrés Felipe <57474509+development-one1@users.noreply.github.com>
2024-09-24 19:23:17 +0200

    Add files via upload

---
+++ src/components/PokemonList.jsx
@@ -0,0 +1,23 @@
+import PokemonCard from './PokemonCard';
+
+const PokemonList = ({ pokemons }) => {
+    return (
+        <div className='PokemonList'>
+            {pokemons.map((pokemon) => {
+                return <PokemonCard
+                    name={pokemon.name}
+                    key={pokemon.name}
+                    image={pokemon.sprites.front_default}
+                    types={pokemon.types}
+
+                />;
+            })}
+        </div>
+    );
+};
+
+PokemonList.defaultProps = {
+    pokemons: Array(10).fill(''),
+};
+
+export default PokemonList;
